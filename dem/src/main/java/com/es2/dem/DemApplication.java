26272023e15e0ccf2d4ec6f2e76ef756496caddd
Eduarda Gotuzzo <m.gotuzzo@edu.pucrs.br>
2025-05-14 20:19:08 -0300

    microsservicos

---
+++ dem/src/main/java/com/es2/dem/DemApplication.java
@@ -0,0 +1,13 @@
+package com.es2.dem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemApplication.class, args);
+	}
+
+}
---
+++ dem/src/test/java/com/es2/dem/DemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2.dem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
